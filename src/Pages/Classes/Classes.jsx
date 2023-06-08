9dca4291f35576298ace7bb642f4c27390e65a6d
jahirul94 <jahirulislam41185@gmail.com>
2023-06-08 09:00:29 +0600

    added student selected class component

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,29 +1,53 @@
 import { useLoaderData } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const Classes = () => {
-    const classes = useLoaderData();
-    const  handleEnrollClass = enrollClass =>{
-       console.log(enrollClass);
-    }
-    
-    return (
-        <div className="grid gap-4 px-6 sm:grid-cols-1 lg:grid-cols-3">
-             {
-                classes.map(singleClass =><div key={singleClass._id} className="card card-compact bg-base-100 shadow-xl">
-                <figure><img src={singleClass.image} className="h-96 w-full" alt="class pic" /></figure>
-                <div className="card-body">
-                  <h2 className="card-title">{singleClass.className}</h2>
-                  <h2 className="text-lg font-semibold">{singleClass.instructorName}</h2>
-                  <h2 className="text-lg font-semibold">Available Seats : {singleClass.availableSeats}</h2>
-                  <h2 className="text-lg font-semibold">Price : ${singleClass.price}</h2>
-                  <div className="card-actions justify-end">
-                    <button onClick={()=>handleEnrollClass(singleClass)} className="btn btn-primary w-full">Enroll</button>
-                  </div>
-                </div>
-              </div> )
-             }
-        </div>
-    );
+  const { user } = useAuth();
+  const classes = useLoaderData();
+  const handleEnrollClass = enrollClass => {
+    console.log(enrollClass);
+    const {className , image , instructorName , price } = enrollClass ;
+    const enrolledClass = {email : user?.email , className , image , instructorName , price }
+    fetch('http://localhost:5000/classes', {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(enrolledClass)
+    })
+      .then(res => res.json())
+      .then(data => {
+         if(data.insertedId){
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Your Classes has been saved On Cart',
+            showConfirmButton: false,
+            timer: 1500
+          })
+         }
+      })
+  }
+
+  return (
+    <div className="grid gap-4 px-6 sm:grid-cols-1 lg:grid-cols-3">
+      {
+        classes.map(singleClass => <div key={singleClass._id} className="card card-compact bg-base-100 shadow-xl">
+          <figure><img src={singleClass.image} className="h-96 w-full" alt="class pic" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">{singleClass.className}</h2>
+            <h2 className="text-lg font-semibold">{singleClass.instructorName}</h2>
+            <h2 className="text-lg font-semibold">Available Seats : {singleClass.availableSeats}</h2>
+            <h2 className="text-lg font-semibold">Price : ${singleClass.price}</h2>
+            <div className="card-actions justify-end">
+              <button onClick={() => handleEnrollClass(singleClass)} className="btn btn-primary w-full">Enroll</button>
+            </div>
+          </div>
+        </div>)
+      }
+    </div>
+  );
 };
 
 export default Classes;
--- src/Pages/DashBoard/StudentDashboard/StudentDashboard.jsx
+++ src/Pages/DashBoard/StudentDashboard/StudentDashboard.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const StudentDashboard = () => {
+     const {user} = useAuth();
+     const [cartItems , setCartItems] = useState([])
+     useEffect(()=>{
+         fetch(`http://localhost:5000/classes?email=${user?.email}`)
+         .then(res => res.json())
+         .then(data => console.log(data))
+     },[user])
     return (
         <div>
              
